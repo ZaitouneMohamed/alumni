653d722c4412a0362f37bcc665d9e0fd5c15a484
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-05-20 22:54:52 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin/login', function () {
+    return view('admin.login');
+});
+
+Route::post("admin/login", [AuthController::class, "login"])->name("admin.login");
+Route::get("logout", [AuthController::class, "logout"])->name("logout");
+
+Route::resource("post", PostController::class)->only("show");
+
+Route::prefix('admin')->name("admin.")->middleware(["AdminAuthRedirection", 'role:admin'])->group(function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
 });
