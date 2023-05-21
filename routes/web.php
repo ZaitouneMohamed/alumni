cae217deae422b91c32dca26a4c37c2380fab6cc
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-05-21 15:46:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\allController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
@@ -18,15 +19,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile', function () {
+    return view('landing.content.profile');
+});
+
+Route::get('/chat', function () {
+    return view('landing.content.chat');
+})->name("chat");
 
 Route::get('/admin/login', function () {
     return view('admin.login');
 });
 
 Route::post("admin/login", [AuthController::class, "login"])->name("admin.login");
+Route::post("register", [AuthController::class, "register"])->name("register");
 Route::get("logout", [AuthController::class, "logout"])->name("logout");
+Route::get("events", [allController::class, "EvenetsList"])->name("EvenetsList.list");
+Route::get("offres", [allController::class, "OffresList"])->name("offres.list");
+Route::get("actualite", [allController::class, "ActualiteList"])->name("ActualiteList.list");
 
-Route::resource("post", PostController::class)->only("show");
+Route::post("AddComment/{id}", [allController::class, "addComment"])->name("addComment");
+
+Route::resource("post", PostController::class);
 
 Route::prefix('admin')->name("admin.")->middleware(["AdminAuthRedirection", 'role:admin'])->group(function () {
     Route::get('/', function () {
